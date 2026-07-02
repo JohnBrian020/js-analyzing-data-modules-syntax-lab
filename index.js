906f733fba2ec001be61fcc3cee3a5a6a7243acd
JohnBrian020 <john.waweru3@student.moringaschool.com>
2026-07-02 08:16:11 +0300

    Example of data analysis module

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 
-
-
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+require('datejs');
+function combineUsers(...args) {
+    const combinedObject = {
+        users: []
+    };
+    for (const array of args) {
+        combinedObject.users.push(...array);
+    }
+    combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+    return combinedObject;
+}
+
+module.exports = {
+    combineUsers
+};
+
+combineUsers();
+
